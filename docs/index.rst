9f96d85d426df5410b7187fb5fc896cbabc3497b
Leego <leegoldfryd0@gmail.com>
2021-01-13 20:48:42 +0200

    add nojekyll file

---
+++ docs/index.rst
@@ -0,0 +1,26 @@
+.. molecool documentation master file, created by
+   sphinx-quickstart on Thu Mar 15 13:55:56 2018.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to molecool's documentation!
+=========================================================
+
+molecool is a python package which can read in, perform analysis and visualize molecular coordintes.
+
+This is a fake package for a workshop
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   getting_started
+   api
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ molecool/tests/test_molecool.py
@@ -0,0 +1,119 @@
+"""
+Unit and regression test for the molecool package.
+"""
+
+# Import package, test suite, and other packages as needed
+import molecool
+import pytest
+import sys
+import numpy as np
+
+
+def test_calculate_distance():
+    """Test that the calculate distance function calculate what we expect."""
+
+    r1 = np.array([0, 0, 0])
+    r2 = np.array([0, 1, 0])
+
+    expected_distance = 1
+
+    observed_distance = molecool.calculate_distance(r1, r2)
+    assert expected_distance == observed_distance
+
+
+@pytest.fixture
+def methane_molecule():
+    symbols = np.array(["C", "H", "H", "H", "H"])
+    coordinates = np.array(
+        [[1, 1, 1], [2.4, 1, 1], [-0.4, 1, 1], [1, 1, 2.4], [1, 1, -0.4]]
+    )
+    return symbols, coordinates
+
+
+@pytest.mark.parametrize(
+    "p1, p2, p3, expected_value",
+    [
+        (
+            np.array([np.sqrt(2) / 2, np.sqrt(2) / 2, 0]),
+            np.array([0, 0, 0]),
+            np.array([1, 0, 0]),
+            45,
+        ),
+        (np.array([0, 0, -1]), np.array([0, 1, 0]), np.array([1, 0, 0]), 60),
+        (
+            np.array([np.sqrt(3) / 2, (1 / 2), 0]),
+            np.array([0, 0, 0]),
+            np.array([1, 0, 0]),
+            30,
+        ),
+    ],
+)
+def test_calculate_angle_many(p1, p2, p3, expected_value):
+
+    calculated_angle = molecool.calculate_angle(p1, p2, p3, degrees=True)
+
+    assert expected_value == pytest.approx(
+        calculated_angle
+    ), f"{calculated_angle} {expected_value}"
+
+
+def test_calculate_angle():
+
+    r1 = np.array([0, 0, -1])
+    r2 = np.array([0, 0, 0])
+    r3 = np.array([1, 0, 0])
+
+    expected_value = 90
+
+    calculated_value = molecool.calculate_angle(r1, r2, r3, degrees=True)
+
+    assert expected_value == calculated_value
+
+def test_molecular_mass():
+    symbols = ['C', 'H', 'H', 'H', 'H']
+
+    calculated_mass = molecool.calculate_molecular_mass(symbols)
+
+    actual_mass = 16.04
+
+    assert pytest.approx(actual_mass, abs=1e-2) == calculated_mass
+
+
+def test_build_bond_list_failure():
+    coordinates = np.array([])
+
+    with pytest.raises(ValueError):
+        molecool.build_bond_list(coordinates)
+
+
+def test_center_of_mass(methane_molecule):
+    symbols, coordinates = methane_molecule
+
+    center_of_mass = molecool.calculate_center_of_mass(symbols, coordinates)
+
+    expected_center = np.array([1, 1, 1])
+
+    assert np.array_equal(center_of_mass, expected_center)
+
+
+@pytest.mark.skip
+def test_build_bond_list_failure(methane_molecule):
+    symbols, coordinates = methane_molecule
+
+    with pytest.raises(ValueError):
+        bonds = molecool.build_bond_list(coordinates, min_bond=-1)
+
+
+def test_build_bond_list(methane_molecule):
+    symbols, coordinates = methane_molecule
+    bonds = molecool.build_bond_list(coordinates)
+
+    assert len(bonds) == 4
+
+    for bond_length in bonds.values():
+        assert bond_length == 1.4
+
+
+def test_molecool_imported():
+    """Sample test, will always pass so long as import statement worked"""
+    assert "molecool" in sys.modules
